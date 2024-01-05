b463101920d912d3c6ad031a7f4badb963a6d7a0
Scott Edwards <scotteza@gmail.com>
2024-01-05 16:25:04 +0000

    Add invariant to loader function and add error handling

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,13 +1,19 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
 
 import { getContact } from "../data";
 
 import type { ContactRecord } from "../data";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId param");
     const contact = await getContact(params.contactId);
+    if (!contact) {
+        throw new Response("Not Found", { status: 404 });
+    }
     return json({ contact });
 };
 
