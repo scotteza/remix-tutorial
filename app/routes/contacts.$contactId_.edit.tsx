8ba128c4cb443f939a52ae392100980bbc7ddc18
Scott Edwards <scotteza@gmail.com>
2024-01-16 17:12:34 +0000

    Add "cancel" button functionality to edit screen

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,6 +1,10 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import {
+    Form,
+    useLoaderData,
+    useNavigate,
+} from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { getContact, updateContact } from "../data";
@@ -28,6 +32,7 @@ export const loader = async ({
 
 export default function EditContact() {
     const { contact } = useLoaderData<typeof loader>();
+    const navigate = useNavigate();
 
     return (
         <Form id="contact-form" method="post">
@@ -77,7 +82,7 @@ export default function EditContact() {
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button onClick={() => navigate(-1)} type="button">Cancel</button>
             </p>
         </Form>
     );
